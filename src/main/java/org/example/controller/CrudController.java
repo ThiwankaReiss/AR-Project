953a5ec6a69c6c782fe95737a195ae7bc37babd4
ThiwankaReiss <thiwankar2003@gmail.com>
2024-05-21 11:19:20 +0530

    Created CrudService and CrudController

---
+++ src/main/java/org/example/controller/CrudController.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudController <dto,entity>{
+    entity save (dto dto) ;
+    boolean delete(Long value) ;
+    List<dto> getAll() ;
+    Long getNextId() throws SQLException;
+    dto getById(Long id);
+}
---
+++ src/main/java/org/example/service/CrudService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudService  <dto,entity>{
+    entity save (dto dto) ;
+    boolean delete(Long value) ;
+    List<dto> getAll() ;
+    Long getNextId() throws SQLException;
+    dto getById(Long id);
+}
